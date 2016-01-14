dea4f80e3a6b60e432514baa0db9ed401b67cd1b
Taryn Parker <tarynp@Taryns-MacBook-Air.local>
2016-01-14 12:09:50 -0600

    test push

--- Calculatron/ViewController.m
+++ Calculatron/ViewController.m
@@ -22,6 +22,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)onOperationTapped:(UIButton *)sender {
+    double op1 = self.operandOne.text.doubleValue;
+    double op2 = self.operandTwo.text.doubleValue;
+    
 }
 
 
